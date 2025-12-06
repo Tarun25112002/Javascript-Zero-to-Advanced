dd6f76be8b898bd655c8b8e19d3aecc68d26b27e
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2025-12-06 15:45:34 +0530

    destructuringofArray

---
+++ destructuringofArray.js
@@ -0,0 +1,8 @@
+let names = ["Tarun", "Tanvi", "Sunita", "Dhirendra"]
+// 1) Extract normal strings (parents contains strings)
+// const [myName, sisName, ...parents] = names;
+
+// 2) Convert each element into an array, so destructuring assigns arrays
+// const [myName, sisName, ...parents] = names.slice(0).map(n => [n]);
+
+// console.log(myName);
